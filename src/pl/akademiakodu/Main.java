623ad20ab71682e6ad5c2de8c986969c3ed51bce
Oskar Pol <oskarbank@gmail.com>
2017-03-14 18:22:34 +0100

    Second

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-         Zadanie9 zadanie9 = new Zadanie9();
-         System.out.println(zadanie9.jestSzescianem(8));
+        // Zadanie9 zadanie9 = new Zadanie9();
+        // System.out.println(zadanie9.jestSzescianem(8));
+
+        Zadanie10 zadanie10 = new Zadanie10();
+        System.out.println("Max to: " + zadanie10.znajdzMaksimum(15,25));
 
     }
 }
---
+++ src/pl/akademiakodu/Zadanie10.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu;
+
+/**
+ * Created by OskarPraca on 2017-03-14.
+ */
+public class Zadanie10 {
+
+     public int znajdzMaksimum(int a, int b){
+         return a > b ? a : b;
+     }
+
+}
